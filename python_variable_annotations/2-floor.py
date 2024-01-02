2c0e136fae2c1ff0473b3bfa502b02e6076ecf2e
Letho1972 <ericmarescq@gmail.com>
2024-01-02 11:49:28 +0100

    Basic annotations - floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+""" Write a type-annotated function floor which takes a float n """
+
+
+def floor(n: float) -> int:
+
+    """
+    Return the floor of the input float.
+
+    Args:
+    - n (float): Input float.
+
+    Returns:
+    - int: Floor of the input float.
+    """
+    return (int(n))
