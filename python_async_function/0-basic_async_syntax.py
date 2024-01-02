5136d377ef7b168dc2fc015942c4ff05f9a8b936
Letho1972 <ericmarescq@gmail.com>
2024-01-02 15:37:59 +0100

    The basics of async annonate

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -6,25 +6,38 @@
 import asyncio
 import random
 
-# Definition of the asynchronous coroutine
+"""Definition of the asynchronous coroutine"""
 
 
 async def wait_random(max_delay: float = 10) -> float:
-    # Generates a random delay between 0 and max_delay as a float value
+
+    """Generates a random delay between 0 and max_delay as a float value"""
+
     delay = random.uniform(0, max_delay)
 
-    # Waits for the generated duration
+    """ Waits for the generated duration"""
+
     await asyncio.sleep(delay)
 
-    # Returns the generated delay
+    """Returns the generated delay"""
+
     return delay
 
-# Main function to run the coroutine in the asyncio event loop
+"""Main function to run the coroutine in the asyncio event loop"""
 
 
 async def main():
-    # Calls and awaits the result of the wait_random coroutine
+
+    """Calls and awaits the result of the wait_random coroutine"""
+
     result = await wait_random()
 
-    # Prints the generated random delay
+    """Prints the generated random delay"""
+
     print(f"Random delay: {result} seconds")
+
+    """Runs the asyncio event loop with the main function"""
+
+    if __name__ == "__main__":
+
+        asyncio.run(main())
