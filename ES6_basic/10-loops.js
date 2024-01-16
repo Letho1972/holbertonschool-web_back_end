9f72f332972336434d8ad0711f75b59eec270bf5
Letho1972 <ericmarescq@gmail.com>
2024-01-16 10:57:28 +0100

    For...of Loops

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+export default function appendToEachArrayValue(array, appendString) {
+  for (let i = 0; i < array.length; i++) {
+    array[i] = appendString + array[i];
+  }
+
+  return array;
+}
