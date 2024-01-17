2a9f3e637816587d7f479b161b9f7ec3e6430e62
Letho1972 <ericmarescq@gmail.com>
2024-01-17 10:54:59 +0100

    Methods, static methods, computed methods dollars

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,27 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+  // Implement a method named displayFullCurrency
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
