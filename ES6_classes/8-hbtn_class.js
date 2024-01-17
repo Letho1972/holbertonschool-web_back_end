e49ce6cd4c3da007e5eeb7109f70e984f3138558
Letho1972 <ericmarescq@gmail.com>
2024-01-17 15:29:45 +0100

    Return the size and location

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,24 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  // Casting to Number returns the size
+  valueOf() {
+    return this._size;
+  }
+
+  // Casting to String returns the location
+  toString() {
+    return this._location;
+  }
+}
