ce998bc13f0e42e2c840631e4846310d7905770f
Letho1972 <ericmarescq@gmail.com>
2024-01-25 13:19:32 +0100

    Return a promise

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,14 @@
+// Return a promise and the parameter is a boolean.
+
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
