8d9f10715d69319dba4c38b48c9f7170dfc8d50c
Letho1972 <ericmarescq@gmail.com>
2024-01-25 13:30:31 +0100

    Throw error / try catch

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,16 @@
+// This function should create and return an array.
+
+export default function guardrail(mathFunction) {
+  const queue = [];
+
+  try {
+    const result = mathFunction();
+    queue.push(result);
+  } catch (error) {
+    queue.push(error.message);
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+
+  return queue;
+}
