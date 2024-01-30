5ca28b5b996a89188c7aa3d5c15ff7f53b0769d7
Letho1972 <ericmarescq@gmail.com>
2024-01-30 11:14:10 +0100

    Map of groceries

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,12 @@
+export default function groceriesList() {
+  const groceryMap = new Map([
+    ['Apples', 10],
+    ['Bananas', 10],
+    ['Milk', 1],
+    ['Bread', 1],
+    ['Eggs', 5],
+    // Add more items as needed
+  ]);
+
+  return groceryMap;
+}
