e9a32275d895dd3a44cd30c29fce40fdc325dc19
Letho1972 <ericmarescq@gmail.com>
2024-05-27 14:52:22 +0200

    fix requirements

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -12,12 +12,12 @@ class BaseCaching:
 
 class BasicCache(BaseCaching):
     def put(self, key, item):
-        """Assigner la valeur item à la clé key dans self.cache_data."""
+        """Assign the value item to the key key in self.cache_data."""
         if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
-        """Retourner la valeur dans self.cache_data associée à key."""
+        """Return the value in self.cache_data associated with key."""
         if key is None or key not in self.cache_data:
             return None
         return self.cache_data[key]
