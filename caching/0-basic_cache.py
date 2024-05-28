452af179620daa704aff49ebf9d52d96d1fe787d
Letho1972 <ericmarescq@gmail.com>
2024-05-28 09:20:48 +0200

    0-basic - 1

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -6,7 +6,9 @@
 class BaseCaching:
 
     def __init__(self):
-        """This caching system doesn’t have limit."""
+        """A documentation is not a simple word, it’s a real sentence explaining
+        what’s the purpose of the module,
+        class or method (the length of it will be verified)"""
         self.cache_data = {}
 
 
