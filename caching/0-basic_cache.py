cec1cabe79f6353d5afa4cf9b1db9aa783eeb1df
Letho1972 <ericmarescq@gmail.com>
2024-05-28 09:22:32 +0200

    0-basic 1_2

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
-""" BaseCaching module
+
+
+""" A documentation is not a simple word, it’s a real sentence explaining
+        what’s the purpose of the module,
+        class or method (the length of it will be verified)
 """
 
 
