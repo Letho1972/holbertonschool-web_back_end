11fef0b7b821f0a21b8f74e885e94b40fdd93db6
Letho1972 <ericmarescq@gmail.com>
2024-05-30 15:38:09 +0200

    3. LRU Caching self.stack remove and append

---
+++ caching/3-lru_cache.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+
+"""Create a class LRUCache that inherits
+from BaseCaching and is a caching system"""
+
+
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """ A class LRUCache that inherits from BaseCaching """
+
+    def __init__(self):
+        """ Initialize the FIFOCache """
+        super().__init__()
+        self.stack = []
+
+    def put(self, key, item):
+        """ Assign to the dictionary self.cache_data the item value """
+        if key is None or item is None:
+            return
+
+        elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            last_key = self.stack.pop(0)
+            del self.cache_data[last_key]
+            print(f"DISCARD: {last_key}")
+
+        self.stack.append(key)
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Return the value in self.cache_data linked to key """
+        if key is None or key not in self.cache_data:
+            return None
+
+        """ Update the order to reflect recent use """
+
+        self.stack.remove(key)
+        self.stack.append(key)
+
+        return self.cache_data[key]
