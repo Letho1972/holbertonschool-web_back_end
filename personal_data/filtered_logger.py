bbbd1e07ec8499ff44308990887b0e3c313af153
Letho1972 <ericmarescq@gmail.com>
2024-06-17 15:42:31 +0200

    fixed task 0

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,17 +4,17 @@
 0. Regex-ing
 Write a function called filter_datum
 that returns the log message obfuscated
-
 """
 
 import re
 from typing import List
 
 
-def filter_datum(fields: list[str], redaction: str,
+def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
-    """ Returns the log message obfuscated """
-
+    """
+    Returns the log message obfuscated
+    """
     return re.sub(f'({"|".join(fields)})=[^{separator}]*',
                   lambda match: match.group(0).split('=')[0] + f'={redaction}',
                   message)
