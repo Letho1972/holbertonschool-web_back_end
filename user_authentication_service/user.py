99f8853e031342e098719b1d65d65b6ce552ee75
Letho1972 <ericmarescq@gmail.com>
2024-06-24 14:30:06 +0200

    add comment

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -4,16 +4,16 @@
 for a database table named users
 (by using the mapping declaration of SQLAlchemy).
 """
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+
 
 Base = declarative_base()
-"""class User(Base)
-"""
+"""class User(Base)"""
 
 
 class User(Base):
+    """class User"""
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
