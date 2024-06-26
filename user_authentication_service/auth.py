0cef153506bb46b3506b6643307a3e7911822530
Letho1972 <ericmarescq@gmail.com>
2024-06-26 18:49:13 +0200

    fixed  hash password

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -1,13 +1,58 @@
+#!/usr/bin/env python3
+"""Hash password"""
+
 import bcrypt
 
-"""define a _hash_password method
-"""
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
-def _hash_password(password: str) -> bytes:
-    """Generate a salt
+class Auth:
+    """Auth class to interact with the authentication database.
     """
-    salt = bcrypt.gensalt()
-    # Hash the password with the generated salt
-    hashed_password = bcrypt.hashpw(password.encode(), salt)
-    return hashed_password
+
+    def __init__(self):
+        """initialization"""
+        self._db = DB()
+
+    def _hash_password(password: str) -> bytes:
+        """
+        Hashes a password using bcrypt.
+
+        Args:
+        password (str): The password to hash.
+
+        Returns:
+        bytes: The hashed password.
+        """
+        return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+    def register_user(self, email: str, password: str) -> User:
+        """Registrer user"""
+        try:
+            """Try to find the user by email"""
+            self._db.find_user_by(email=email)
+            """if email already existe"""
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            """if no email found, we do the registration"""
+            hashed_password = self._hash_password(password)
+            """Add user to the database and return the User object"""
+            user = self._db.add_user(
+                email=email, hashed_password=hashed_password)
+            return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        Method taht expect email and password required arguments
+        and return a boolean
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user and bcrypt.checkpw(password.encode('utf-8'),
+                                       user.hashed_password):
+                return True
+        except NoResultFound:
+            pass
+        return False
