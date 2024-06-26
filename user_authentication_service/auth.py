83424ac3df0833ab46f4be87c684b47a3bc0ab91
Letho1972 <ericmarescq@gmail.com>
2024-06-26 16:18:54 +0200

    All your functions should be type annotated

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -1,8 +1,12 @@
 import bcrypt
 
+"""define a _hash_password method
+"""
+
 
 def _hash_password(password: str) -> bytes:
-    # Generate a salt
+    """Generate a salt
+    """
     salt = bcrypt.gensalt()
     # Hash the password with the generated salt
     hashed_password = bcrypt.hashpw(password.encode(), salt)
