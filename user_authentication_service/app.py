f979c4ad7a3449966d361eb210eb5fffc5280d4f
Letho1972 <ericmarescq@gmail.com>
2024-06-27 11:39:46 +0200

    add commits

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,9 +1,13 @@
 from flask import Flask, jsonify
 
+""" Set up a basic Flask app """
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
+    """ a single GET route ("/")"""
+
     return jsonify({
         'message': 'Hello, World!',
         'status': 'success'
