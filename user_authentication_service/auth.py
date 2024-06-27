2bc21f80e87a587d91b511019bbf447ac79cbb32
Letho1972 <ericmarescq@gmail.com>
2024-06-27 10:23:44 +0200

    fixed task 4

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python3
 """Hash password"""
-
 import bcrypt
-
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
 
 
+def _hash_password(password: str) -> bytes:
+    """
+    Hashes a password using bcrypt.
+    Args:
+    password (str): The password to hash.
+    Returns:
+    bytes: The hashed password.
+    """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
-
     def __init__(self):
         """initialization"""
         self._db = DB()
@@ -31,30 +39,17 @@ class Auth:
                 email=email, hashed_password=hashed_password)
             return user
 
-    def _hash_password(self, password: str) -> bytes:
-        """
-        Hashes a password using bcrypt.
-
-        Args:
-        password (str): The password to hash.
-
-        Returns:
-        bytes: The hashed password.
-        """
-        return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-
     def valid_login(self, email: str, password: str) -> bool:
         """
         Method taht expect email and password required arguments
         and return a boolean
         """
+
         try:
             user = self._db.find_user_by(email=email)
             if user and bcrypt.checkpw(password.encode('utf-8'),
                                        user.hashed_password):
-
                 return True
-
         except NoResultFound:
             pass
         return False
