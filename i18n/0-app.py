d67240df06bd2da20974aecd6af554187651a332
Letho1972 <ericmarescq@gmail.com>
2024-07-03 11:17:41 +0200

    fixed 0 app.py

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python3
-""" setup a basic Flask app """
-from flask import Flask
+""" Route module for the API """
+from flask import Flask, request, render_template
+from os import getenv
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
-@app.route('/')
-def home():
-    return "Hello, World!"
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """ GET /
+    Return:
+      - 0-index.html
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
