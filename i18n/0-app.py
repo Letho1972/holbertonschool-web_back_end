c910c68ecb289212e5047ea38e733310e4be6286
Letho1972 <ericmarescq@gmail.com>
2024-07-03 11:06:24 +0200

    0. Basic Flask app

---
+++ i18n/0-app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" setup a basic Flask app """
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, World!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
