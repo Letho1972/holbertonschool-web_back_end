c43a2eaafc0f1b9b843b7c1f134dce1ec3061d0d
Letho1972 <ericmarescq@gmail.com>
2024-07-16 10:44:20 +0200

    fixed  output task 0  change code

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -3,20 +3,23 @@ const assert = require('assert');
 const calculateNumber = require("./0-calcul.js");
 
 // test cases
+
 describe('calculateNumber', () => {
-    it('should return 4 when a is 1 and b is 3', () => {
-        assert.strictEqual(calculateNumber(1, 3), 4);
+    it('checks the output', () => {
+      assert.strictEqual(calculateNumber(1, 3), 4);
+      assert.strictEqual(calculateNumber(1, 3.7), 5);
+      assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+      assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+      assert.strictEqual(calculateNumber(3.7, 1), 5);
+      assert.strictEqual(calculateNumber(3.7, 1.2), 5);
     });
-
-    it('should return 5 when a is 1 and b is 3.7', () => {
-        assert.strictEqual(calculateNumber(1, 3.7), 5);
+    it('negative numbers', () => {
+      assert.strictEqual(calculateNumber(-1, 1), 0);
+      assert.strictEqual(calculateNumber(1, -1), 0);
+      assert.strictEqual(calculateNumber(-1, -1), -2);
     });
-
-    it('should return 5 when a is 1.2 and b is 3.7', () => {
-        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    it('checks arguments', () => {
+      assert.strictEqual(isNaN(calculateNumber(1)), true);
+      assert.strictEqual(isNaN(calculateNumber()), true);
     });
-
-    it('should return 6 when a is 1.5 and b is 3.7', () => {
-        assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-    });
-});
+  });
