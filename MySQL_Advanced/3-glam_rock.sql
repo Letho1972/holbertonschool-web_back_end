f76bd886727773acb6e9b4d7926644b13295cf90
Letho1972 <ericmarescq@gmail.com>
2024-08-19 18:40:14 +0200

    3. Old school band

---
+++ MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,5 @@
+-- 3. Old school band
+-- Lists all bands with Glam rock as their main style, ranked by longevity
+SELECT band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%';
